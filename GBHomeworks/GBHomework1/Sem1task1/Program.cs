51e681842d5252b5dcdba9a6995e275d6895653a
Anatoly <anatoly.biz@gmail.com>
2022-10-20 20:47:32 +0300

    добавили решение задачи 1

---
+++ GBHomeworks/GBHomework1/Sem1task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a < b)
+{
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
