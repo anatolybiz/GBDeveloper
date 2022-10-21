ca5090d18241c9b1664db917ff28ae33be22a4c9
Anatoly <anatoly.biz@gmail.com>
2022-10-21 18:44:58 +0300

    Добавили программу сложения рандомных чисел

---
+++ GBLections/Lection1/Example003_Sum/Random_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// new Random().Next(min, max) даст случайное целое число от min до max-1 (за то, что это будут целые числа отвечает Next)
+int numberA = new Random().Next(1, 10); // продолжение. [min, max-1] или [min, max) . 1,2,3,4 ... 9 - макс будет 9
+Console.WriteLine(numberA); // показать какое число было загадано для numberA
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB); // показать какое число было загадано для numberB
+int result = numberA + numberB;
+Console.WriteLine(result);
