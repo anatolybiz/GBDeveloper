849b046a8f5675341826cb5776975326774c8342
Anatoly <anatoly.biz@gmail.com>
2022-10-21 19:11:52 +0300

    Добавили задачу с условием if и else

---
+++ GBLections/Lection1/Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша") // дополнение ToLower() позволяет принимать написанное имя в любом регистре, а не только Маша
+{
+    Console.WriteLine("Ура, это же МАША!");
+}    
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
