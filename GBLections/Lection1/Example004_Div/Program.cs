e4b885928fbfacbeafdee132a8ea512645e99b2b
Anatoly <anatoly.biz@gmail.com>
2022-10-21 17:45:46 +0300

    Добавили пример деления двух чисел

---
+++ GBLections/Lection1/Example004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿// double - переменная типа данных(контейнер)б число с остатком, а не только целое
+// деление двух чисел
+Console.WriteLine("Введите первое число "); //показывает текст Введите первое число
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Частное двух чисел составляет: {numberA / numberB}");
