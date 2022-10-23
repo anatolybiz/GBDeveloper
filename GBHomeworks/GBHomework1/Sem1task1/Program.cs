961d4d9e465d1098f0f38db7ecdc67c39d30cc05
Anatoly <anatoly.biz@gmail.com>
2022-10-23 22:07:29 +0300

    Добавили решение 1 задачи

--- GBHomeworks/GBHomework1/Sem1task1/Program.cs
+++ GBHomeworks/GBHomework1/Sem1task1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.Write("Введите первое число: ");
-int a = int.Parse(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 
 if (a < b)
 {
