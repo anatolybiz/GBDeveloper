f309d5b79901ad4abdde129ba7ffa902c1d8d011
Anatoly <anatoly.biz@gmail.com>
2022-10-23 22:54:12 +0300

    Добавили решение задачи 2

--- GBHomeworks/GBHomework1/Sem1task2/Program.cs
+++ GBHomeworks/GBHomework1/Sem1task2/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+    Console.WriteLine("Максимальное число = " + a);
+else
+if (b > c)
+    Console.WriteLine("Максимальное число = " + b);
+else
+    Console.WriteLine("Максимальное число = " + c);
