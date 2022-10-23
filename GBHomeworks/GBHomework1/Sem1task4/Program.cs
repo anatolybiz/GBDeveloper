17ed4e887746c09a1d523ba65f467e0cb2e7cef9
Anatoly <anatoly.biz@gmail.com>
2022-10-23 23:39:34 +0300

    Добавили решение задачи 4

---
+++ GBHomeworks/GBHomework1/Sem1task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 0)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+} 
+else
+if (n < 0)
+{
+    for (int i = 1; i >= n; i--)
+    {
+        if (i % 2 == 0 && i != 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
