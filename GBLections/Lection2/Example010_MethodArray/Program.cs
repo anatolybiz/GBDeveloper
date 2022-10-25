29d74ec01c4fcdb028bd66ff4c4b50a9e23775c8
Anatoly <anatoly.biz@gmail.com>
2022-10-25 21:08:12 +0300

    Добавлена лекция 2

---
+++ GBLections/Lection2/Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length; //Length возвращает длину или кол-во элементов массива
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //команда Прерви работу
+    }
+    index = index +1;
+    // index++;
+}
---
+++ GBLections/Lection2/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)  //если метод ничего не возвращает, он называется void методом
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //сначала определили массив из 10 элементов
+
+FillArray(array); //вызвали метод FillArray который заполнил нам массив
+
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array); //метод PrintArray массив распечатал в консоль
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
