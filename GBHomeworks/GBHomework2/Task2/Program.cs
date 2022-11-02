6ac2756315c9d03c112772dd34a31d1c504a7cc3
anatolybiz <anatoly.biz@gmail.com>
2022-11-02 23:20:34 +0300

    Добавлено решение второй задачи

---
+++ GBHomeworks/GBHomework2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать третью цифру из трехзначного числа, либо вывести на экран, что ее нет
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99)
+{
+    Console.WriteLine(n > 99 ? n.ToString()[2] : '-');
+
+}
+else if (n < -99)
+{
+    Console.WriteLine(n < -99 ? n.ToString()[3] : '-');
+
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
