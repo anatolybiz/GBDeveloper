de809de35885c9507728435be80dcd5ad51da413
anatolybiz <anatoly.biz@gmail.com>
2022-11-02 23:07:25 +0300

    Добавлено решение задачи 1

---
+++ GBHomeworks/GBHomework2/Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿//программа выводящая второе число из трехзначного
+Console.WriteLine("Enter three number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = (n - ((n / 100)) * 100) / 10;
+Console.WriteLine($"Изначальное число {n}, вторая цифра {result}");
