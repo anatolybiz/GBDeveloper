9a7795010caa0aa108e48c7c0e7caf1355c7218b
anatolybiz <anatoly.biz@gmail.com>
2022-11-04 17:06:07 +0300

    скорректировали решение задач

---
+++ GBHomeworks/GBHomework2/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine());
+
+switch (a)
+{
+    case 1: Console.WriteLine("Понедельник, рабочий день"); break;
+    case 2: Console.WriteLine("Вторник, рабочий день"); break;
+    case 3: Console.WriteLine("Среда, рабочий день"); break;
+    case 4: Console.WriteLine("Четверг, рабочий день"); break;
+    case 5: Console.WriteLine("Пятница, рабочий день"); break;
+    case 6: Console.WriteLine("Суббота, выходной"); break;
+    case 7: Console.WriteLine("Воскресение, выходной"); break;
+}
