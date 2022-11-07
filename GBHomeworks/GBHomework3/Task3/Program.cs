4b84a0a68c1ba17f503e7928fb8a0a0f53d8e6ea
anatolybiz <anatoly.biz@gmail.com>
2022-11-07 15:25:12 +0300

    Добавлено решение задачи 3

---
+++ GBHomeworks/GBHomework3/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1 ; i <= number ; i++)
+{
+    Console.Write($"{Math.Pow(i , 3)}  ");
+}
