3f23005514fe51338627fbebcf61e5bfb4e4e1ce
anatolybiz <anatoly.biz@gmail.com>
2022-11-08 14:36:58 +0300

    Добавлено решение 1 задачи

---
+++ GBHomeworks/GBHomework5/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] CreateArray(int size)
+{
+	int[] array = new int[size];
+        Console.Write("Массив [");
+    	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = new Random().Next(100, 1000);
+        Console.Write($"{array[i]}  ");
+	}
+    Console.Write("]");
+    return array;
+	}
+
+int HowManyEvenNumbers(int[] array)
+{
+	int evenNumber = 0;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+			if (array [i] % 2 == 0)
+				evenNumber++;
+            else evenNumber = evenNumber;
+	}
+
+	return evenNumber;
+}
+
+
+int[] array = CreateArray(10);
+Console.WriteLine();
+Console.WriteLine($"Количество четных значений: " + (HowManyEvenNumbers(array)));
