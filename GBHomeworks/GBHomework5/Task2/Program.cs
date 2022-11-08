c0e7cdb9ad770b1e48f1cde598f012871f2cfc59
anatolybiz <anatoly.biz@gmail.com>
2022-11-08 14:45:12 +0300

    Добавлено решение задачи 2

---
+++ GBHomeworks/GBHomework5/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArray(int size)
+{
+	int[] array = new int[size];
+        Console.Write("Массив [");
+    	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = new Random().Next(1, 10);
+        Console.Write($"{array[i]}  ");
+	}
+    Console.Write("]");
+    return array;
+	}
+
+int SummElements(int[] array)
+{
+	int sum = 0;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+			if (i % 2 == 1 && i!=0)
+				sum = sum + array[i];
+			else 
+			    sum = sum;
+	}
+
+	return sum;
+}
+
+
+int[] array = CreateArray(5);
+Console.WriteLine();
+Console.WriteLine($"Cумма значений элементов нечётных позиций: " + (SummElements(array)));
