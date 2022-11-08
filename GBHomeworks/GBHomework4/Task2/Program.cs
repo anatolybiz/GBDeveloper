9a500965fd13778f6cf3195b881bdead8083784c
anatolybiz <anatoly.biz@gmail.com>
2022-11-08 14:03:38 +0300

    добавлено решение второй задачи

---
+++ GBHomeworks/GBHomework4/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+string str = n.ToString();
+int[] b = new int[str.Length];
+int result = 0;
+for( int i=0; i< str.Length; i++)
+{
+     b[i] = int.Parse(str[i].ToString());
+     result = result + b[i];
+}
+Console.WriteLine(result);
