1bb17dfc87141ae836e512f768ceb4889f41710f
anatolybiz <anatoly.biz@gmail.com>
2022-11-12 17:50:47 +0300

    добавлено решение задачи 47

---
+++ GBHomeworks/GBHomework7/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+
+double[,] GetArray(int m, int n)
+
+{
+    double[,] array = new double[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(rnd.Next(-100, 100) + rnd.NextDouble(), 2);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write($"{array[i,j]} ");
+        }
+       Console.WriteLine();
+    } 
+    
+}
+PrintArray(GetArray(m,n));
